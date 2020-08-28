9df2e8072cf52678f1d34fa493f97edd0eac4b40
Vishal Chowdhary <vishalchowdhary@Vishal-Chowdhary.local>
2020-08-28 12:04:27 +0530

    #saveTestCases

--- src/index.js
+++ src/index.js
@@ -1,43 +1,6 @@
-const express = require("express");
-require("./db/mongoose.js");
-const userRouter = require('./routers/user.js')
-const taskRouter = require('./routers/task.js')
+const app = require('./app')
 
-
-
-const app = express();
 const port = process.env.PORT;
-
-const multer = require('multer')
-
-const upload = multer({ 
-    dest:'images'
-})
-
-app.post('/upload',upload.single('upload'),(req,res)=>{
-
-    res.send()
-
-
-})
-
-app.use(express.json());
-
-// app.use((req,res,next)=>{
-//     res.status(503).send("Site is currently down, Check back soon!")
-// })
-// app.use((req,res,next)=>{
-//     console.log(req.method,req.path)
-//     if(req.method==='GET'){
-//         res.send('Get Method Disabled!')
-//     }else{
-//         next()
-//     }
-// })
-
-app.use(userRouter)
-app.use(taskRouter)
-
 
 app.listen(port, () => {
     console.log("server is up on port " + port);
